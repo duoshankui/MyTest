088dc7d9f4d20c5531c42be1f936decb87a0e7b5
DooubleK <2578138035@qq.com>
2018-11-12 23:47:55 +0800

    张三新增AddView

---
+++ HelloDemo/HelloDemo/AddView.swift
@@ -0,0 +1,21 @@
+//
+//  AddView.swift
+//  HelloDemo
+//
+//  Created by DoubleK on 2018/11/12.
+//  Copyright © 2018 DoubleK. All rights reserved.
+//
+
+import UIKit
+
+class AddView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
