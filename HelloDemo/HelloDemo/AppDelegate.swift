1d6c33faf70b47885f121c979c270e5db861bbba
DoubleK <2578138035@qq.com>
2018-11-18 22:11:20 +0800

    dev1 分支修改

--- HelloDemo/HelloDemo/AppDelegate.swift
+++ HelloDemo/HelloDemo/AppDelegate.swift
@@ -16,6 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+      print("修改dev1")
         return true
     }
 
