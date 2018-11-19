3a8c54889e8573e5388fb3e24db0312138bbc47a
DoubleK <2578138035@qq.com>
2018-11-19 10:55:08 +0800

    另一个用户新增NewDemoView

---
+++ HelloDemo/HelloDemo/NewDemoView.swift
@@ -0,0 +1,21 @@
+//
+//  NewDemoView.swift
+//  HelloDemo
+//
+//  Created by DoubleK on 2018/11/19.
+//  Copyright © 2018年 DoubleK. All rights reserved.
+//
+
+import UIKit
+
+class NewDemoView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
